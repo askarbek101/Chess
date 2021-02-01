571b3dcf0f0ab28d7bc5af25e6f23a5b93a9f690
alma-iceee <alma.iceee@gmail.com>
2021-02-01 16:17:48 +0600

    Adding Game Engine (copied)

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,76 @@
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable  {
 
+    private boolean isRunning = false;
+    private Thread thread;
+
     public Game() {
         new Window(800, 800, "sandbox", this);
+        start();
+    }
+
+    private void start() {
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    private void stop() {
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
 
+        while (isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            render();
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+            }
+        }
+
+        stop();
+    }
+
+    public void tick() { }
+
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 800, 800);
+
+
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String[] args) {
