421d1aa83a2026d3da8ace2565a711d88afa7fe3
alma-iceee <alma.iceee@gmail.com>
2021-02-01 16:13:58 +0600

    Creating Window with JFrame library

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,17 @@
-public class Game {
-    public static void main(String[] args) { }
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable  {
+
+    public Game() {
+        new Window(800, 800, "sandbox", this);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
 }
